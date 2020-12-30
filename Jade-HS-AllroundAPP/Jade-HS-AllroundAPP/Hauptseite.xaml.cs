9d1041ec5c1bdf48297297e9f9227c7fe8ba1728
JanRipken <72017165+JanRipken@users.noreply.github.com>
2020-12-30 19:24:56 +0100

    Initial
    Login seite
    
    grundgerüst hauptseite

---
+++ Jade-HS-AllroundAPP/Jade-HS-AllroundAPP/Hauptseite.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Jade_HS_AllroundAPP
+{
+    /// <summary>
+    /// Interaktionslogik für Hauptseite.xaml
+    /// </summary>
+    public partial class Hauptseite : Window
+    {
+        public Hauptseite()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btn_maximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void btn_Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void btn_Menü_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Jade-HS-AllroundAPP/Jade-HS-AllroundAPP/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+namespace Jade_HS_AllroundAPP
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)            
+                DragMove();            
+        }
+
+    
+
+        private void SCHLISSEN_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void LOGIN_Click(object sender, RoutedEventArgs e)
+        {
+            Hauptseite hauptseite = new Hauptseite();
+            hauptseite.Show();
+            this.Close();
+        }
+
+        private void REGISTRIEREN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
